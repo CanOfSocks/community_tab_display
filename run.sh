0ecc026db2036731bdaf20743c0f907ad988e648
unknown <github@canofsocks.com>
2025-12-23 20:09:15 +1100

    Use variables for gunicorn arguments

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-exec gunicorn -w 4 -b 0.0.0.0:9000 --keep-alive 5 --max-requests 1000 --max-requests-jitter 100 --chdir /app/  app:app
+#!/bin/sh
+exec gunicorn \
+  -w ${GUNICORN_WORKERS:-4} \
+  -b ${GUNICORN_BIND:-0.0.0.0:9000} \
+  --keep-alive ${GUNICORN_KEEP_ALIVE:-5} \
+  --max-requests ${GUNICORN_MAX_REQUESTS:-1000} \
+  --max-requests-jitter ${GUNICORN_MAX_REQUESTS_JITTER:-100} \
+  --chdir /app/ \
+  app:app
+
